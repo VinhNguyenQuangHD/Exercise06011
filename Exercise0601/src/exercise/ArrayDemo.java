044392158a0e099ba1fff200118fa02dce20c9d6
VinhNguyenQuangHD <61536993+VinhNguyenQuangHD@users.noreply.github.com>
2022-05-07 20:38:31 +0700

    add

---
+++ Exercise0601/src/exercise/ArrayDemo.java
@@ -0,0 +1,15 @@
+package exercise;
+
+public class ArrayDemo {
+    public static void main(String[] args){
+        int[] arrayInt = new int[5];
+        arrayInt[0] = 1;
+        arrayInt[1] = 2;
+        arrayInt[2] = 3;
+        arrayInt[3] = 4;
+        arrayInt[4] = 5;
+
+        System.out.println("Array length: " + arrayInt.length);
+        System.out.println("Element at index 3 is :" + arrayInt[3]);
+    }
+}
